c96e20e14525dc820d2ae662324ad850af106a38
Nam Giang <kyng@ece.ubc.ca>
2016-03-20 15:24:36 -0700

    adding more files

--- index.js
+++ index.js
@@ -1,59 +1,21 @@
-var mongoRead = require('./mongoout')('mongodb://localhost:27017/cpen400')
-var express = require('express')
-var bodyParser = require('body-parser');
-var app = express()
+var express = require('express'),
+    // fs = require('fs'),
+    app = express(),
+    // https = require('https').createServer({key: fs.readFileSync('key.pem'),cert: fs.readFileSync('cert.pem')}, app),
+    http = require('http').createServer(app);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000));
+// app.set('sport', (process.env.PORT || 443));
+app.use(express.static(__dirname + '/public'));
 
-// mongoRead.initDb();
+app.get('/index', function(request, response) {
+    response.send('Hi World');
+});
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// https.listen(app.get('sport'), function() {
+//   console.log("Node app is running at :" + app.get('sport'));
+// });
 
-app.get('/products', function(request, response) {
-  var token = request.query.token;
-
-  mongoRead.validate(token, function(success){
-    if (!success)
-      response.status(401).send();
-    else {
-      response.header("Access-Control-Allow-Origin", "*");
-      response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      
-      mongoRead.getAll(mongoRead.PRODUCTS, function(products){
-        if (!products)
-          response.status(502).send();
-        else
-          response.json(products);
-      })
-    }
-  })
-})
-
-app.post('/checkout', function(request, response) {
-  // console.log(response.data)
-  var token = request.body.token;
-
-  mongoRead.validate(token, function(success){
-    if (!success)
-      response.status(401).send();
-    else {
-      response.header("Access-Control-Allow-Origin", "*");
-      response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      var cartTotal = request.body.total
-      var cartString = request.body.cart
-
-      mongoRead.addOrder(cartString, cartTotal);
-      var dec = mongoRead.decrementCount(cartString)
-      response.send('DONE');
-    }
-  })
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+http.listen(app.get('port'), function() {
+  console.log("Node app is running at :" + app.get('port'));
+});
